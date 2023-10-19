6414fc25ff03630e77c4320e49d7bb50ccae7dd5
jinzzang <fullpee@gmail.com>
2023-10-19 10:19:32 +0900

    Update Config.java

--- src/main/java/com/example/vueProject/config/Config.java
+++ src/main/java/com/example/vueProject/config/Config.java
@@ -13,7 +13,7 @@ public class Config implements WebMvcConfigurer {
         // 모든 경로에 대해
         registry.addMapping("/**")
                 // Origin이 http:localhost:20010에 대해
-                .allowedOrigins("http://localhost:20010")
+                .allowedOrigins("http://192.168.56.1:20010")
                 // GET, POST, PUT, PATCH, DELETE, OPTIONS 메서드를 허용한다.
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
